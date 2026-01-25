4dd5a5f314088d5b46fb20f2f1eb051bd1d294a9
shravani-smd <shraumd@gmail.com>
2026-01-25 19:30:44 +0530

    Add files via upload
    Upload blog website files

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function toggleDark() {
+    document.body.classList.toggle("dark");
+    localStorage.setItem("theme",
+        document.body.classList.contains("dark") ? "dark" : "light");
+}
+
+window.onload = () => {
+    if (localStorage.getItem("theme") === "dark") {
+        document.body.classList.add("dark");
+    }
+};
